e5c4814d7d4bdf721be6e0895f5f426f0e6cca6e
bhanuo07 <rathoreb853@gmail.com>
2024-07-25 17:55:08 +0530

    Checking Pipeline

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Added a comment
   return (
     <html className="hydrated" lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
