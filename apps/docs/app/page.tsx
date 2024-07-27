777a467e72cb0b0ac4ea35bde55d40b22f109234
Bhanu Rathore <rathoreb853@gmail.com>
2024-07-27 17:25:11 +0530

    Update page.tsx : Removed a console

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@repo/ui/shadcn/button";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("HEll", styles.page);
+
   return (
     <div className="flex h-full w-full flex-1 bg-red-200">
       <Button>Button from the docs</Button>
