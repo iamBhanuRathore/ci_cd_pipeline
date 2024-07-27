0642931097bd078a8ae5c78a50ad35845c9840d4
bhanuofficepes <bhanuofficepes@gmail.com>
2024-07-27 08:25:07 +0000

    Added CD pipeline

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -26,5 +26,5 @@ app.post("/", async (req, res) => {
   });
 });
 app.listen(5000, () => {
-  console.log("Server is Running on Port: 5000");
+  console.log("Server is Running on Port: 5000 http://localhost:5000");
 });
